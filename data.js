c260fad45e0f384e797f63bc3e179cfc0372cfd0
Markus Tarn <Markus.Tarn@myjar.com>
2021-03-25 21:57:55 +0200

    Added maps

--- data.js
+++ data.js
@@ -10,7 +10,13 @@ data = [
 			"bottom": "green darken-3"
 		},
 		"motto": "Eesti tütar, Eesti kodu tugevasti seotud olgu",
-		"tartu": "Tartu, Struwe 4",
+		"location": {
+			"tartu": {
+				"address": "Wilhelm Struve 4",
+				"lat": "58.377148927506795",
+				"long": "26.72170246377966"
+			}
+		},
 		"member": "fiilia",
 		"sex": "female",
 		"url": "http://www.cfp.ee/",
@@ -27,7 +33,13 @@ data = [
 			"bottom": "green darken-3"
 		},
 		"motto": "Sõnasse kindlust, teosse ausust, kodule armastust",
-		"tartu": "Tartu, Tähtvere 4",
+		"location": {
+			"tartu": {
+				"address": "Tartu, Tähtvere 4",
+				"lat": "58.383010279406136",
+				"long": "26.713507568933963"
+			}
+		},
 		"member": "indlaensis",
 		"sex": "female",
 		"url": "http://www.cfp.ee/",
@@ -44,7 +56,13 @@ data = [
 			"bottom": "purple lighten-1"
 		},
 		"motto": "Constantia ad finem ducit (Püsivus viib sihile)",
-		"tallinn": "Kaarli pst 5-2",
+		"location": {
+			"tallinn": {
+				"address": "Kaarli pst 5-2",
+				"lat": "59.43214962456414",
+				"long": "24.741793980989513"
+			}
+		},
 		"member": "lembelensa",
 		"sex": "female",
 		"url": "http://www.lembela.ee",
@@ -61,7 +79,13 @@ data = [
 			"bottom": "orange accent-1"
 		},
 		"motto": "Per aspera ad astra (Läbi raskuste tähtede poole)",
-		"tartu": "Kastani 65-2",
+		"location": {
+			"tartu": {
+				"address": "Kastani 65-2",
+				"lat": "58.372144910528576",
+				"long": "26.713557519003647"
+			}
+		},
 		"member": "amica",
 		"sex": "female",
 		"url": "http://www.amicitia.ee",
@@ -78,8 +102,18 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Teadmised on ilu",
-		"tartu": "Lai 30",
-		"tallinn": "Raekojaplats 16",
+		"location": {
+			"tartu": {
+				"address": "Lai 30",
+				"lat": "58.38358373751969",
+				"long": "26.719261125632865"
+			},
+			"tallinn": {
+				"address": "Raekojaplats 16",
+				"lat": "59.43726830950381",
+				"long": "24.746035478503995"
+			}
+		},
 		"member": "estonica",
 		"sex": "female",
 		"url": "http://www.sororitasestoniae.ee",
@@ -96,8 +130,18 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Ühisus, kindlus, ausus!",
-		"tartu": "Lai 30",
-		"tallinn": "Uus 28/2-14",
+		"location": {
+			"tartu": {
+				"address": "Lai 30",
+				"lat": "58.38358373751969",
+				"long": "26.719261125632865"
+			},
+			"tallinn": {
+				"address": "Uus 28/2-14",
+				"lat": "59.4409364311802",
+				"long": "24.749916472224186"
+			}
+		},
 		"member": "vironus",
 		"sex": "male",
 		"url": "https://www.vironia.ee",
@@ -115,7 +159,18 @@ data = [
 		},
 		"motto": "Teos õiglane ja vahvas, ustav sulle, eesti rahvas",
 		"tartu": "Vanemuise 54",
-		"tallinn": "Pikk 39",
+		"location": {
+			"tartu": {
+				"address": "Vanemuise 54",
+				"lat": "58.37273513820852",
+				"long": "26.71381857432409"
+			},
+			"tallinn": {
+				"address": "Pikk 39",
+				"lat": "59.43940490733648",
+				"long": "24.746583852666106"
+			}
+		},
 		"member": "frater esticus",
 		"sex": "male",
 		"url": "https://www.cfe.ee",
@@ -132,8 +187,18 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Üks kõigi eest, kõik ühe eest",
-		"tartu": "Veski 69",
-		"tallinn": "Roosikrantsi 3",
+		"location": {
+			"tartu": {
+				"address": "Veski 69",
+				"lat": "58.376917581492194",
+				"long": "26.71161582546655"
+			},
+			"tallinn": {
+				"address": "Roosikrantsi 3",
+				"lat": "59.43236414541949",
+				"long": "24.74364873905055"
+			}
+		},
 		"member": "sakalanus",
 		"sex": "male",
 		"url": "https://www.sakala.ee",
@@ -150,7 +215,13 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Isamaa, sõprus, ausus!",
-		"tartu": "Kuperjanovi 16",
+		"location": {
+			"tartu": {
+				"address": "Julius Kuperjanovi 16",
+				"lat": "58.37636855927431",
+				"long": "26.71384062461321"
+			}
+		},
 		"member": "ugalensis",
 		"sex": "male",
 		"url": "https://korpugala.ee",
@@ -167,8 +238,18 @@ data = [
 			"bottom": "green darken-3"
 		},
 		"motto": "Eestimaa, mu isamaa",
-		"tartu": "Tähe 3",
-		"tallinn": "Pikk 11",
+		"location": {
+			"tartu": {
+				"address": "Tähe 3",
+				"lat": "58.37311784696499",
+				"long": "26.724994989099716"
+			},
+			"tallinn": {
+				"address": "Pikk 11",
+				"lat": "59.43808341124439",
+				"long": "24.744485935158547"
+			}
+		},
 		"member": "rotalus",
 		"sex": "male",
 		"url": "https://rotalia.ee",
@@ -185,8 +266,18 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Vendlus, ausus, hoolsus!",
-		"tartu": "Jakobi 52",
-		"tallinn": "A. Weizenbergi 10/1-6",
+		"location": {
+			"tartu": {
+				"address": "Jakobi 52",
+				"lat": "58.38261006019194",
+				"long": "26.712328394030084"
+			},
+			"tallinn": {
+				"address": "A. Weizenbergi 10/1-6",
+				"lat": "59.438599117257695",
+				"long": "24.781647208386918"
+			}
+		},
 		"member": "frater liviensis",
 		"sex": "male",
 		"url": "https://liviensis.ee",
@@ -203,7 +294,13 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Truudus, tublidus, ausus",
-		"tallinn": "Tatari 12",
+		"location": {
+			"tallinn": {
+				"address": "Tatari 12",
+				"lat": "59.43057475167708",
+				"long": "24.747196626105573"
+			}
+		},
 		"member": "leolensis",
 		"sex": "male",
 		"url": "https://www.leola.ee",
@@ -220,8 +317,18 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Üks kõige – kõik ühe eest",
-		"tartu": "Veski 45",
-		"tallinn": "Raekojaplats 16",
+		"location": {
+			"tartu": {
+				"address": "Veski 45",
+				"lat": "58.378464246258254",
+				"long": "26.710292352295454"
+			},
+			"tallinn": {
+				"address": "Raekojaplats 16",
+				"lat": "59.43726830950381",
+				"long": "24.746035478503995"
+			}
+		},
 		"member": "revelus",
 		"sex": "male",
 		"url": "https://www.revelia.ee",
@@ -238,7 +345,13 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Kindlus, ausus, vendlus!",
-		"tallinn": "Pikk 29a",
+		"location": {
+			"tallinn": {
+				"address": "Pikk 29a",
+				"lat": "59.4389315552539",
+				"long": "24.746131758498294"
+			}
+		},
 		"member": "tehnolus",
 		"sex": "male",
 		"url": "https://www.tehnola.ee",
@@ -255,8 +368,18 @@ data = [
 			"bottom": "purple darken-3"
 		},
 		"motto": "Pidevas töös, tõe valguses, elurõõmsalt tulevikku!",
-		"tartu": "Jaani 20",
-		"tallinn": "Raekojaplats 16",
+		"location": {
+			"tartu": {
+				"address": "Jaani 20",
+				"lat": "58.38310388004305",
+				"long": "26.718563962091125"
+			},
+			"tallinn": {
+				"address": "Raekojaplats 16",
+				"lat": "59.43726830950381",
+				"long": "24.746035478503995"
+			}
+		},
 		"member": "frater tartuensis",
 		"sex": "male",
 		"url": "https://tartuensis.ee",
@@ -273,7 +396,13 @@ data = [
 			"bottom": "yellow accent-4"
 		},
 		"motto": "Deus, Libertas, Patria! (Jumal, vabadus, isamaa!)",
-		"tartu": "Õpetaja 11",
+		"location": {
+			"tartu": {
+				"address": "Õpetaja 11",
+				"lat": "58.374620535491346",
+				"long": "26.71581828007436"
+			}
+		},
 		"member": "arminus",
 		"sex": "male",
 		"url": "http://www.arminia.ee",
@@ -290,6 +419,7 @@ data = [
 			"bottom": "red darken-3"
 		},
 		"motto": "P.C.T (Рцы слово твердо)",
+		"location": {},
 		"member": "",
 		"sex": "male",
 		"url": "https://www.arctica.lv",
@@ -306,8 +436,18 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "Tugevalt asjaajamises, meeldival viisil",
-		"tartu": "Jaan Tõnissoni 1",
-		"tallinn": "Johann Köleri 13",
+		"location": {
+			"tartu": {
+				"address": "Jaan Tõnissoni 1",
+				"lat": "58.37646700715399",
+				"long": "26.710511986441787"
+			},
+			"tallinn": {
+				"address": "J. Köleri 13",
+				"lat": "59.43740879050934",
+				"long": "24.779377118916027"
+			}
+		},
 		"member": "seltsivend",
 		"sex": "male",
 		"url": "http://www.eys.ee",
@@ -324,8 +464,18 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Töö isamaa kasuks",
-		"tartu": "Jakobi 23",
-		"tallinn": "Roosikrantsi 8b",
+		"location": {
+			"tartu": {
+				"address": "Jakobi 23",
+				"lat": "59.42791279657787",
+				"long": "24.770783581087958"
+			},
+			"tallinn": {
+				"address": "Roosikrantsi 8b",
+				"lat": "59.43096665465064",
+				"long": "24.743331995178796"
+			}
+		},
 		"member": "põhjalane",
 		"sex": "uni",
 		"url": "http://www.põhjala.ee",
@@ -342,7 +492,13 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Ex solo ad solem soli patriae",
-		"tartu": "K.E. von Baeri tn 7",
+		"location": {
+			"tartu": {
+				"address": "Karl Ernst von Baeri 7",
+				"lat": "58.38170772568812",
+				"long": "26.712692158143124"
+			}
+		},
 		"member": "liiviklane",
 		"sex": "male",
 		"url": "https://liivika.ee",
@@ -359,7 +515,13 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Demokraatia, rahvuslikkus, individuaalsus",
-		"tartu": "Riia 13-33",
+		"location": {
+			"tartu": {
+				"address": "Riia 13-33",
+				"lat": "58.37376552408489",
+				"long": "26.72131011452072"
+			}
+		},
 		"member": "-",
 		"sex": "female",
 		"url": "https://enys.ee",
@@ -376,7 +538,13 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Õige vaimsuse kaudu vendlusele, nende mõlema kaudu vabadusele!",
-		"tartu": "Gildi 3-10",
+		"location": {
+			"tartu": {
+				"address": "Gildi 3-10",
+				"lat": "58.38151242557774",
+				"long": "26.721341941710666"
+			}
+		},
 		"member": "veljestolane",
 		"sex": "male",
 		"url": "https://www.arctica.lv",
@@ -393,7 +561,13 @@ data = [
 			"bottom": "black"
 		},
 		"motto": "Demokraatia, isiksus, isamaa",
-		"tartu": "Veski 42",
+		"location": {
+			"tartu": {
+				"address": "Veski 42",
+				"lat": "58.37689151594549",
+				"long": "26.71257482392983"
+			}
+		},
 		"member": "raimlane",
 		"sex": "male",
 		"url": "https://raimla.ee",
@@ -410,7 +584,13 @@ data = [
 			"bottom": "white"
 		},
 		"motto": "-",
-		"tartu": "Jaani 4",
+		"location": {
+			"tartu": {
+				"address": "Jaani 4",
+				"lat": "58.3820727587017",
+				"long": "26.719683754933037"
+			}
+		},
 		"member": "fennicus",
 		"sex": "uni",
 		"url": "http://ff.ee",
